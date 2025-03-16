4128c6ed8968f6e34ee424bf323389276a0f45a5
n-i-c-k-n-a-m-e <shabanov.ragim@list.ru>
2025-03-16 21:27:01 +0300

    uploading

---
+++ Lesson1/phoneandtabletmodule/src/main/java/ru/shabanovrv/phoneandtabletmodule/MainActivity.java
@@ -0,0 +1,34 @@
+package ru.shabanovrv.phoneandtabletmodule;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.textViewStudent);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № X");
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+}
