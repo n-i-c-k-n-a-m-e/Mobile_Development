d2563e3c862e9d791ccca50fc45217c3296826c3
n-i-c-k-n-a-m-e <shabanov.ragim@list.ru>
2025-05-01 18:34:24 +0300

    Updating

---
+++ Lesson2/app/src/main/java/ru/mirea/shabanovrv/lesson2/MainActivity.java
@@ -0,0 +1,31 @@
+package ru.mirea.shabanovrv.lesson2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        String data = "M I R E A";
+        data += "- best";
+        tv.setText(data);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//               return insets;
+    //    });
+    }
+}
---
+++ Lesson2/settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lesson2"
+include(":app")
+include(":activitylifecycle")
+include(":MultiActivity")
+include(":multiactivity")
+include(":multiactivity2")
+include(":intentfilter")
+include(":toastapp")
+include(":notificationapp")
+include(":dialog")
